35cc384276bbd51195731ee5180a38a69b7805b7
stefanocravotta <you@example.com>
2022-05-27 12:05:25 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp />
+    <MainComp />
+    <FooterComp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
+import FooterComp from './components/FooterComp.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderComp,
+    MainComp,
+    FooterComp
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style/vars";
+@import "./assets/style/global";
+@import "./assets/style/mixins";
 </style>
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+      F
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterComp"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
