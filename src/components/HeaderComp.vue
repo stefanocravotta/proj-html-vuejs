10b84bc4a0d1584b2ffec9b4d06ca4ef74baee5e
stefanocravotta <you@example.com>
2022-05-27 12:17:07 +0200

    header init

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,24 @@
 <template>
   <header>
-      H
+      <div class="container d-flex justify-content-between">
+        <div class="logo">Logo</div>
+        <nav>
+            <ul class="d-flex">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+            </ul>
+        </nav>
+      </div>
   </header>
 </template>
 
@@ -11,5 +29,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/vars";
 
+header{
+    height: 80px;
+    padding: 15px 0;
+    background-color: $sugar-cane;
+}
 </style>
